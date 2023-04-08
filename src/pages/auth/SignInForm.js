c93bab2d55a5cfdf45fba2c9caa5436245bf0839
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-08 16:30:25 +0000

    feat: Add form from react-bootstrap and customize it as needs

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,9 +15,38 @@ function SignInForm() {
     <Col className="my-auto p-0 p-md-2">
       <Container className={`${styles.Container} p-4 `}>
         <h1 className={styles.Header}>sign in</h1>
-        {/*form here */}
+
+        <Form>
+          <Form.Group controlId="username">
+            <Form.Label className="d-none">Username</Form.Label>
+            <Form.Control
+              className={styles.Input}
+              type="text"
+              placeholder="Enter your username"
+              name="username"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label className="d-none">Password</Form.Label>
+            <Form.Control
+              className={styles.Input}
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+            />
+          </Form.Group>
+
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Wide}`}
+            type="submit"
+          >
+            Sign in
+          </Button>
+        </Form>
       </Container>
-      <Container className={`mt-3 ${styles.Container}`}>
+
+      <Container className="mt-3">
         <Link className={styles.Link} to="/signup">
           Don't have an account? <span>Sign up now!</span>
         </Link>
