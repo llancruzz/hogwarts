6e20be58b9a7e50cc409143d1aa57e89721a35d7
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-08 10:20:51 +0000

    feat: Add logo and favicon to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
     <Navbar fixed="top" expand="md">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo hogwarts" height="100" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Sign in</Nav.Link>
+            <Nav.Link>Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
