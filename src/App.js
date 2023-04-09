b15093904e98d12b07d2353c7b12104b04df83be
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-09 08:34:17 +0000

    feat: Create export CurrentUserContext, SetCurrentUserContext and wrap them on return components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+
+/* 
+useContext()provides a way to pass through the component tree without having to pass props down manually at every level.
+Create and export the CurrentUserContext and setCurrentUserContext by calling the createContext function.
+*/
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
   /*
@@ -28,18 +35,31 @@ function App() {
     }
   };
 
+  /*
+  To have code run when a component mounts, make use of the useEffect hook and pass it an empty dependency array. 
+  Call the handleMount function inside. 
+  */
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  // Providers will allow both the currentUser value and the function to update it, to be available to every child component in application.
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route render={() => <p>Page Not Fond!</p>} />
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home Page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page Not Fond!</p>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
