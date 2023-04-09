0dc41f92f0c9e7bbc38efe0a5e20f71b5cd1e179
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-09 08:01:02 +0000

    feat: Create handleMount async to get the request to the dj-rest-auth/user/user/ endpoint of api

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  /*
+  Add the functionality to  check for the users' logged in status, so that  
+  it can display different links in the navbar  depending on if the user is logged in or not. 
+  */
+  const [currentUser, setCurrentUser] = useState(null);
+
+  /*
+  Make a network request to check  who the user is, based on their credentials in the cookie.
+  Make a GET  request to the dj-rest-auth/user/ endpoint of API and make it when the component mounts. 
+  */
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm/>} />
-          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page Not Fond!</p>} />
         </Switch>
       </Container>
