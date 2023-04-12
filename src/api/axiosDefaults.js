b9f02605c4fa04f4f8fec2fb11699f485503a249
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-12 08:14:49 +0000

    feat: Set up tokens using useMemo library

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,11 @@ import axios from "axios";
 axios.defaults.baseURL = "https://hogwarts-hp.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+/*
+Refreshing access tokens:
+Export two newly created axios instances that we’ll attach the interceptors to.  
+One to intercept the request and the  other one to intercept the response. 
+*/
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,8 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { axiosRes } from "../api/axiosDefaults";
+import { response } from "msw";
+import { useHistory } from "react-router-dom";
 
 /* 
 useContext()provides a way to pass through the component tree without having to pass props down manually at every level.
@@ -7,6 +10,8 @@ Create and export the CurrentUserContext and setCurrentUserContext by calling th
 */
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
+
+// Create two custom hooks to make accessing currentUser and setCurrentUser.
 
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
@@ -17,6 +22,9 @@ export const CurrentUserProvider = ({ children }) => {
   it can display different links in the navbar  depending on if the user is logged in or not. 
   */
   const [currentUser, setCurrentUser] = useState(null);
+
+  // Redirect the user to the SignIn page if the refresh token fails.
+  const history = useHistory();
 
   /*
   Make a network request to check  who the user is, based on their credentials in the cookie.
@@ -38,6 +46,32 @@ export const CurrentUserProvider = ({ children }) => {
   useEffect(() => {
     handleMount();
   }, []);
+
+  /*
+  useMemo() hook used here to cache complex values that take time to compute.
+  It runs before the childrean components are mounted. Attach the interceptors before the children mount.
+  */
+  useMemo(() => {
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  });
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
