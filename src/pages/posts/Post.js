39106ece11a8083a6ecc770289b5106fd96c86c5
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-13 10:21:43 +0000

    feat: Cread card to post component and add styles

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,8 +1,48 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ProfilePicture from "../../components/ProfilePicture";
 
 const Post = (props) => {
-  return <div>Post text here</div>;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    likes_count,
+    like_id,
+    title,
+    content,
+    image,
+    house,
+    updated_at,
+  } = props;
+
+  /*
+  Define a currentUser variable using the useCurrentUser hook exported from the currentUserContexts file.
+  Check if the owner of the post matches the currentUser’s username, and assign the returned boolean value to the is_owner variable.
+  */
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <ProfilePicture src={profile_image} height={80} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+          </div>
+        </Media>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
