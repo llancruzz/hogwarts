a0b239571f9daadbe8221afe000eeadd7e65a3ad
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 16:06:20 +0000

    feat: Create handleDelete function to allow user delete their own comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -5,10 +5,20 @@ import { Link } from "react-router-dom";
 import ProfilePicture from "../../components/ProfilePicture";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 // Create Comment component so that users can display  the comments they created.
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
 
   /*
   Define a currentUser variable using the useCurrentUser hook exported from the currentUserContexts file.
@@ -16,6 +26,33 @@ const Comment = (props) => {
   */
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  /*
+ Handle delete function:
+ Make request api axiosRes to comment id to delete it.
+ Decrement comments_count by one. Filter out the deleted comment from the array.
+ Update both with setPost and setComments.
+ */
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -31,7 +68,7 @@ const Comment = (props) => {
           <p>{content}</p>
         </Media.Body>
         {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
         )}
       </Media>
     </>
