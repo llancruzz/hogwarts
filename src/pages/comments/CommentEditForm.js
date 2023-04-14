4fd9672a67d6710cef7e0d3ad594ee03e7fe238d
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 16:28:24 +0000

    feat: Create handleChage to allow user edit its own comments

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
+// Destructure
 function CommentEditForm(props) {
+  const { id, content, setShowEditForm, setComments } = props;
+  const [formContent, setFormContent] = useState(content);
+
+  // Create handleChange to get value of comments.
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
   return (
     <Form>
       <Form.Group className="pr-1">
-        <Form.Control className={styles.Form} as="textarea" rows={2} />
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          rows={2}
+          value={formContent}
+          onChange={handleChange}
+        />
       </Form.Group>
       <div className="text-right">
         <Button className={btnStyles.Button} type="button">
