93d8e664a0e852c96b1b17ab50764f23d1cca8bd
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 15:45:55 +0000

    feat: Add MoreDropdown component to comment component

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,19 @@ import styles from "../../styles/Comment.module.css";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ProfilePicture from "../../components/ProfilePicture";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
+// Create Comment component so that users can display  the comments they created.
 const Comment = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
+
+  /*
+  Define a currentUser variable using the useCurrentUser hook exported from the currentUserContexts file.
+  Check if the currently logged in user is the owner of a comment.
+  */
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <>
@@ -20,6 +30,9 @@ const Comment = (props) => {
           <span className={styles.Date}>{updated_at}</span>
           <p>{content}</p>
         </Media.Body>
+        {is_owner && (
+          <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+        )}
       </Media>
     </>
   );
