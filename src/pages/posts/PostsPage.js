39ed257f239d18ce8f5a1f4083e4be599dbf7bae
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 09:15:35 +0000

    feat: Add adjust align items center to asset

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -35,6 +35,18 @@ function PostsPage({ message, filter = "" }) {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+
         {/* List of posts : map over posts and render each one, show no results asset and show loading spinner */}
         {hasLoaded ? (
           <>
