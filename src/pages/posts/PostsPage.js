2e8705ca372a4b22d210783623c5a913b1c4523e
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 08:10:44 +0000

    feat: Destructure message,filter and useEffect to fetch filter posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasloaded] = useState(false);
+  const [house, setHouse] = useState(null);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasloaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasloaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
