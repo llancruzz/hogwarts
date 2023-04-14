5474d0091633800ac1f93a2f77305620d4e30714
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 14:12:44 +0000

    feat: Add styles to form comment

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -1,25 +1,33 @@
 import React from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ProfilePicture from "../../components/ProfilePicture";
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 const CommentCreateForm = (props) => {
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+    <Form className={`mt-2 ${styles.Form}`}>
+      <Form.Group>
+        <InputGroup>
+          <Link>
+            <ProfilePicture />
+          </Link>
+          <Form.Control
+            className={styles.Comment}
+            placeholder="Say something"
+            as="textarea"
+            rows={2}
+          />
+        </InputGroup>
       </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button
+        className={`${btnStyles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        post
       </Button>
     </Form>
   );
