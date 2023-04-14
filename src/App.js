ab8e14da18d10295c9839d67192a81ec68447a71
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 07:46:57 +0000

    feat: Create PostsPage component and routes for feed and liked posts

--- src/App.js
+++ src/App.js
@@ -7,15 +7,52 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostsPage from "./pages/posts/PostsPage";
 
 function App() {
+  /*
+  Create routes to know who the currentUser is, so we can return the posts they liked, and the ones by profiles they follow.
+  Set the currentUser value by calling and auto-importing the useCurrentUser hook. 
+  Get the profile_id, to know whose profile_id to filter the posts by. 
+  In case the currentUser’s details are still being fetched in the background, it will default to an empty string. 
+  */
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   // Providers will allow both the currentUser value and the function to update it, to be available to every child component in application.
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
