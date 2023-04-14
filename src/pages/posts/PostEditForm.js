17ee02e3d1cc0f4fcefd6662a53636a9be9af2ed
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 13:38:04 +0000

    feat: Change create text on button to save and remove ternary conditional image from form group

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -4,11 +4,9 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Upload from "../../assets/upload.png";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Asset";
 import { useHistory, useParams } from "react-router-dom";
 import { Alert, Image } from "react-bootstrap";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -184,7 +182,7 @@ function PostEditForm() {
         cancel
       </Button>
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-        create
+        save
       </Button>
     </div>
   );
@@ -197,29 +195,17 @@ function PostEditForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              {/* Add a ternary to show a preview of user's image if they have chosen one */}
-              {image ? (
-                <>
-                  <figure>
-                    <Image className={appStyles.Image} src={image} rounded />
-                  </figure>
-                  <div>
-                    <Form.Label
-                      className={`${btnStyles.Button} ${btnStyles.Wide}`}
-                      htmlFor="image-upload"
-                    >
-                      Change the image
-                    </Form.Label>
-                  </div>
-                </>
-              ) : (
+              <figure>
+                <Image className={appStyles.Image} src={image} rounded />
+              </figure>
+              <div>
                 <Form.Label
-                  className="d-flex justify-content-center"
+                  className={`${btnStyles.Button} ${btnStyles.Wide}`}
                   htmlFor="image-upload"
                 >
-                  <Asset src={Upload} message="Click to upload an image" />
+                  Change the image
                 </Form.Label>
-              )}
+              </div>
 
               <Form.File
                 id="image-upload"
