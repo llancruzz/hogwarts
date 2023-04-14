f771dc1ad609a6a64f68e3209bbc39f516eba8e5
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 09:36:27 +0000

    feat: Add onChange to search bar form to query keywords

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,11 +16,12 @@ function PostsPage({ message, filter = "" }) {
   const [hasLoaded, setHasloaded] = useState(false);
   const [house, setHouse] = useState(null);
   const { pathname } = useLocation();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasloaded(true);
       } catch (err) {
@@ -29,7 +30,7 @@ function PostsPage({ message, filter = "" }) {
     };
     setHasloaded(false);
     fetchPosts();
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
@@ -41,6 +42,8 @@ function PostsPage({ message, filter = "" }) {
           onSubmit={(event) => event.preventDefault()}
         >
           <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
             type="text"
             className="mr-sm-2"
             placeholder="Search posts"
