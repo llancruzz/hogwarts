b4bcc2dfcdd62e4f7420f9a4743a8b9dbe683fe7
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-14 14:29:46 +0000

    feat: Create handleSubmit function to allow user comment on the form fields

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import ProfilePicture from "../../components/ProfilePicture";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { axiosRes } from "../../api/axiosDefaults";
 
-const CommentCreateForm = (props) => {
+function CommentCreateForm(props) {
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
 
@@ -17,33 +18,64 @@ const CommentCreateForm = (props) => {
     setContent(event.target.value);
   };
 
-  return (
-    <Form className="mt-2">
-      <Form.Group>
-        <InputGroup>
-          <Link>
-            <ProfilePicture />
-          </Link>
-          <Form.Control
-            className={styles.Comment}
-            placeholder="Say something"
-            as="textarea"
-            value={content}
-            onChange={handleChange}
-            rows={2}
-          />
-        </InputGroup>
-      </Form.Group>
+  /*
+  Form submit handler:
+  Call preventDefault so that the page doesn't refresh.
+  Create async function: inside a try-catch block, make request comment
+  to the endpoint in API application for user comments.
+  */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
 
-      <Button
-        className={`${btnStyles.Button} btn d-block ml-auto`}
-        disabled={!content.trim()}
-        type="submit"
-      >
-        post
-      </Button>
-    </Form>
-  );
-};
+return (
+  <Form className="mt-2" onSubmit={handleSubmit}>
+    <Form.Group>
+      <InputGroup>
+        <Link>
+          <ProfilePicture />
+        </Link>
+        <Form.Control
+          className={styles.Comment}
+          placeholder="Say something"
+          as="textarea"
+          value={content}
+          onChange={handleChange}
+          rows={2}
+        />
+      </InputGroup>
+    </Form.Group>
+
+    <Button
+      className={`${btnStyles.Button} btn d-block ml-auto`}
+      disabled={!content.trim()}
+      type="submit"
+    >
+      post
+    </Button>
+  </Form>
+);
 
 export default CommentCreateForm;
