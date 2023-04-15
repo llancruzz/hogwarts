8fefba1260188199cf983a9428567da629314108
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-15 12:08:02 +0000

    feat: Create handleMount to featch popular profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import appStyles from "../../styles/PopularProfiles.module.css";
 import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
+  // useState hook to store most follwed profiles in the state.
+  const [profileData, setProfileData] = useState({
+    // pageProfile
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
+
+  // Fetch popularProfiles data on  mount using the useEffect hook.
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [currentUser]);
+
   return (
     <Container className={appStyles.Post}>
       <p>Most followed profiles.</p>
+      {popularProfiles.results.map((profile) => (
+        <p key={profile.id}>{profile.owner}</p>
+      ))}
     </Container>
   );
 };
