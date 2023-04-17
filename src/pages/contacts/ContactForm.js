ad07fa8d3f2fbeb172962e3e392af5e0f0bd9ee4
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-17 14:34:34 +0000

    feat: Create handleChange and handleSubmit to contact form

--- src/pages/contacts/ContactForm.js
+++ src/pages/contacts/ContactForm.js
@@ -3,6 +3,7 @@ import { Button, Container, Form, Modal } from "react-bootstrap";
 import styles from "../../styles/PostCreateEditForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const ContactForm = () => {
   /* 
@@ -19,16 +20,61 @@ const ContactForm = () => {
 
   const history = useHistory();
 
+  /*
+  Handle function to handle the inputs field's state changes.
+  Call setContactData and spread the contactData.
+  Create a key value  pair, with the key being the input field name,  
+  and the value being the value entered by the user.
+  */
+  const handleChange = (event) => {
+    setContactData({
+      ...contactData,
+      /* KEY | VALUE */
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  /*
+  Form submit handler:
+  Call preventDefault so that the page doesn't refresh.
+  Create async function: inside a try-catch block, post all the formData to the endpoint in API application for user contacts.
+  Append all two relevant pieces of data: reason_contact and content.
+  */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("reason_contact", reason_contact);
+    formData.append("content", content);
+
+    try {
+      await axiosReq.post("/contacts/", formData);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const textFields = (
     <div className="text-center">
       <Form.Group>
         <Form.Label>Please describe the reason for contacting us:</Form.Label>
-        <Form.Control type="text" name="reason" />
+        <Form.Control
+          type="text"
+          name="reason_contact"
+          value={reason_contact}
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Details:</Form.Label>
-        <Form.Control as="textarea" rows={5} name="content" />
+        <Form.Control
+          as="textarea"
+          rows={5}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Button className={btnStyles.Button}>Cancel</Button>
@@ -40,11 +86,11 @@ const ContactForm = () => {
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Container className={styles.Container}>{textFields}</Container>
       </Form>
       <Modal>
-        <Modal.Header>
+        <Modal.Header closeButton>
           <Modal.Title>Confirmed</Modal.Title>
         </Modal.Header>
         <Modal.Body>Thank you for your feedback!</Modal.Body>
