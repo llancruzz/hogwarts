008dc65cf4d44d6aef023ce7c85c149af1b92c6d
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-18 14:22:01 +0000

    feat: Add badge and styles houses component

--- src/pages/houses/Houses.js
+++ src/pages/houses/Houses.js
@@ -1,50 +1,72 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Badge, Button, Card, Col, Container, Row } from "react-bootstrap";
 import styles from "../../styles/Houses.module.css";
 import gryffindor from "../../assets/gryffindor.webp";
 import slytherin from "../../assets/slytherin.webp";
 import ravenclaw from "../../assets/havenclaw.webp";
 import hufflepuff from "../../assets/hufflepuff.webp";
 import hogwartsAll from "../../assets/hogwartscrest.webp";
-import stylesImage from "../../styles/Image.module.css"
+import stylesImage from "../../styles/Image.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 const Houses = () => {
   return (
     <Container className={styles.Container}>
       <Row>
         <Col md={3} sm={6}>
-          <Card>
+          <Card className={styles.Card}>
             <Card.Img variant="top" src={gryffindor} />
             <Card.Body>
-              <Card.Title>Gryffindor</Card.Title>
-              <Card.Text>This is the content for card 1.</Card.Text>
+              <Badge className={styles.Badge}>
+                <Card.Title>Gryffindor</Card.Title>
+              </Badge>
+
+              <Button className={btnStyles.Button}>
+                <Card.Link href="#">Description</Card.Link>
+              </Button>
+              <Card.Text>Current Points</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3} sm={6}>
-          <Card>
+          <Card className={styles.Card}>
             <Card.Img variant="top" src={slytherin} />
             <Card.Body>
-              <Card.Title>Slytherin</Card.Title>
-              <Card.Text>This is the content for card 2.</Card.Text>
+              <Badge className={styles.Badge}>
+                <Card.Title>Slytherin</Card.Title>
+              </Badge>
+              <Button className={btnStyles.Button}>
+                <Card.Link href="#">Description</Card.Link>
+              </Button>
+              <Card.Text>Current Points</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3} sm={6}>
-          <Card>
+          <Card className={styles.Card}>
             <Card.Img variant="top" src={ravenclaw} />
             <Card.Body>
-              <Card.Title>Ravenclaw</Card.Title>
-              <Card.Text>This is the content for card 3.</Card.Text>
+              <Badge className={styles.Badge}>
+                <Card.Title>Ravenclaw</Card.Title>
+              </Badge>
+              <Button className={btnStyles.Button}>
+                <Card.Link href="#">Description</Card.Link>
+              </Button>
+              <Card.Text>Current Points</Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col md={3} sm={6}>
-          <Card>
+          <Card className={styles.Card}>
             <Card.Img variant="top" src={hufflepuff} />
             <Card.Body>
-              <Card.Title>Hufflepuff</Card.Title>
-              <Card.Text>This is the content for card 4.</Card.Text>
+              <Badge className={styles.Badge}>
+                <Card.Title>Hufflepuff</Card.Title>
+              </Badge>
+              <Button className={btnStyles.Button}>
+                <Card.Link href="#">Description</Card.Link>
+              </Button>
+              <Card.Text>Current Points</Card.Text>
             </Card.Body>
           </Card>
         </Col>
