44c6c0745f7f13241a2b0913a11ed2fc1eee4a2c
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-18 11:09:32 +0000

    feat: Add imagens to each houses of hogwarts card

--- src/pages/houses/Houses.js
+++ src/pages/houses/Houses.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import styles from "../../styles/Houses.module.css"
+import styles from "../../styles/Houses.module.css";
+import gryffindor from "../../assets/gryffindor.webp";
+import slytherin from "../../assets/slytherin.webp";
+import ravenclaw from "../../assets/havenclaw.webp";
+import hufflepuff from "../../assets/hufflepuff.webp";
+import hogwartsAll from "../../assets/hogwartscrest.webp";
+import stylesImage from "../../styles/Image.module.css"
 
 const Houses = () => {
   return (
@@ -8,7 +14,7 @@ const Houses = () => {
       <Row>
         <Col md={3} sm={6}>
           <Card>
-            <Card.Img variant="top" />
+            <Card.Img variant="top" src={gryffindor} />
             <Card.Body>
               <Card.Title>Gryffindor</Card.Title>
               <Card.Text>This is the content for card 1.</Card.Text>
@@ -17,7 +23,7 @@ const Houses = () => {
         </Col>
         <Col md={3} sm={6}>
           <Card>
-            <Card.Img variant="top" />
+            <Card.Img variant="top" src={slytherin} />
             <Card.Body>
               <Card.Title>Slytherin</Card.Title>
               <Card.Text>This is the content for card 2.</Card.Text>
@@ -26,7 +32,7 @@ const Houses = () => {
         </Col>
         <Col md={3} sm={6}>
           <Card>
-            <Card.Img variant="top" />
+            <Card.Img variant="top" src={ravenclaw} />
             <Card.Body>
               <Card.Title>Ravenclaw</Card.Title>
               <Card.Text>This is the content for card 3.</Card.Text>
@@ -35,7 +41,7 @@ const Houses = () => {
         </Col>
         <Col md={3} sm={6}>
           <Card>
-            <Card.Img variant="top" />
+            <Card.Img variant="top" src={hufflepuff} />
             <Card.Body>
               <Card.Title>Hufflepuff</Card.Title>
               <Card.Text>This is the content for card 4.</Card.Text>
@@ -43,6 +49,13 @@ const Houses = () => {
           </Card>
         </Col>
       </Row>
+      <div className={stylesImage.ImageContainer}>
+        <img
+          src={hogwartsAll}
+          className={stylesImage.ImageResponsive}
+          alt="hogwarts all houses"
+        />
+      </div>
     </Container>
   );
 };
