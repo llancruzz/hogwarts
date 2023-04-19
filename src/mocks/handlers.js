bd1355f9f8a91851b00b05a6aab50e09eaa423ab
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-04-19 10:23:49 +0000

    feat: Set up react units tests and create API request handlers

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+// Mock response: url from axios default file.
+const baseURL = "https://hogwarts-hp.herokuapp.com/";
+
+// Store the mocked request handlers in a array.
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 3,
+        username: "lancruz",
+        email: "alancruzsilva@gmail.com",
+        first_name: "",
+        last_name: "",
+        profile_id: 3,
+        profile_image:
+          "https://res.cloudinary.com/llancruzz/image/upload/v1/media/images/profile-pic_yeczcm",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+/*
+Connect mock service worker:
+Create a server instance with all the handlers defined in handlers.js
+Start the server before all tests are run.
+Reset the handlers after each test.
+Colse the server after all the tests are run.
+*/
+import { setupServer } from "msw/node";
+import { handlers } from "./mocks/handlers";
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
